08fb2224776cf913982897af61c9c861b5d0b9d4
TriXXteR <charliehorwood.lbl@gmail.com>
2024-01-01 13:09:55 +0000

    Create FactionUnitMarker.cs

---
+++ Scripts/FactionUnitMarker.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class FactionUnitMarker : MonoBehaviour
+{
+    public int unitIndexToSpawn = -1;
+}
